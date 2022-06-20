e3373c6d437ba37bc2ec3e5a24dee4020406bb7f
Davor <dsekulic1@etf.unsa.ba>
2022-06-20 18:09:06 +0200

    Standalone db

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM eclipse-temurin:11
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8081
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -2,9 +2,7 @@ package com.example.helloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true)
@@ -14,8 +12,4 @@ public class HelloWorldApplication {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
